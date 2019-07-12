97293a3f7a5df01ea62c4392880d6dc90031e6ea
isilva <isilva@scopus.com.br>
2019-07-12 09:48:38 -0300

    Initial commit

---
+++ src/main/java/br/com/treinamento/tdd/modelo/Negociacao.java
@@ -0,0 +1,22 @@
+package br.com.treinamento.tdd.modelo;
+
+import java.time.LocalDateTime;
+
+public class Negociacao {
+
+	private double preco;
+	private int quantidade;
+	private LocalDateTime data;
+	
+	public double getPreco() {
+		return preco;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	
+	
+}
